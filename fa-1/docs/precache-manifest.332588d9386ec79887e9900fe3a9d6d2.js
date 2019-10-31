ea469397c0bc6b815bc862e43aec2b3bc2b4e207
Daan <43338443+DaanKos@users.noreply.github.com>
2019-11-01 00:21:48 +0100

    Alternative way to publish to github pages

---
+++ fa-1/docs/precache-manifest.332588d9386ec79887e9900fe3a9d6d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eaa3e6ae0db863f90e836524b1ff3d",
+    "url": "/frontend-applications/index.html"
+  },
+  {
+    "revision": "ec9001d7d80af245e67d",
+    "url": "/frontend-applications/static/css/main.fd38ed74.chunk.css"
+  },
+  {
+    "revision": "b52dc2175b990bbbdfa6",
+    "url": "/frontend-applications/static/js/2.8d87714a.chunk.js"
+  },
+  {
+    "revision": "ec9001d7d80af245e67d",
+    "url": "/frontend-applications/static/js/main.f4710ee1.chunk.js"
+  },
+  {
+    "revision": "2f9d758cfeb0462c33b7",
+    "url": "/frontend-applications/static/js/runtime-main.17a68829.js"
+  }
+]);
